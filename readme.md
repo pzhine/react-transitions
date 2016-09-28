c11a6088593134d9a5f1646e18310d711c8e466c
Paul <paul@ps-MacBook-Pro.local>
2016-09-28 18:45:26 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Getting started
+`npm install`
+
+To start the app:
+`npm start`
+
+Browse to http://localhost:3000/
---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {items: ['call mom', 'swim', 'eat lunch', 'smile']};
+  },
+  handleAdd: function() {
+    var newItems = this.state.items.concat([prompt('Enter some text')]);
+    this.setState({items: newItems});
+  },
+  render: function() {
+    var items = this.state.items.map((item, i) => {
+      return (
+        <li key={item}>{item}</li>
+      );
+    });
+    return (
+      <div>
+        <button onClick={this.handleAdd}>Add Item</button>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <TodoList />,
+  document.getElementById('content')
+);
