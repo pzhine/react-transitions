a898934bc6dc81219ce8df8e7a671590d87cac8f
Paul <paul@ps-MacBook-Pro.local>
2016-09-28 22:32:33 -0400

    exercise 1 solution

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,27 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 var TodoList = React.createClass({
   getInitialState: function() {
-    return {items: ['call mom', 'swim', 'eat lunch', 'smile']};
+    return {
+      items: ['call mom', 'swim', 'eat lunch', 'smile'],
+      completed: []
+    };
   },
   handleAdd: function() {
     var newItems = this.state.items.concat([prompt('Enter some text')]);
     this.setState({items: newItems});
   },
+  handleClick: function(i) {
+    this.setState({completed: this.state.completed.concat(i)})
+  },
   render: function() {
     var items = this.state.items.map((item, i) => {
       return (
-        <li key={item}>{item}</li>
+        <li key={item} onClick={this.handleClick.bind(this, i)}>
+          {item}
+          <ReactCSSTransitionGroup transitionName="todoDone">
+            {this.state.completed.includes(i) ? (<p>Done</p>) : null}
+          </ReactCSSTransitionGroup>
+        </li>
       );
     });
     return (
